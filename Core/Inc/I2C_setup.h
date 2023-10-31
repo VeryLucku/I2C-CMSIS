fb2c6df5bcfaaa6bbd29eb50b542933a9b422840
Daniil <sukhikhDaniilA@yandex.ru>
2023-10-31 16:28:26 +0300

    Made fixes and delete code not using for I2C init

--- Core/Inc/I2C_setup.h
+++ Core/Inc/I2C_setup.h
@@ -1,22 +1,5 @@
 #include "stm32f4xx.h"
 
 #define SSD1306_I2C_ADDR        (0x3C << 1)
-#define SSD1306_WIDTH 128
-#define SSD1306_Height 32
-#define SSD1306_BUFFER_SIZE SSD1306_WIDTH*SSD1306_Height/8
-
-#define WRITE_COMMAND_MEM_ADDR 0x00
-#define WRITE_DATA_MEM_ADDR 0x40
-
-#define SSD1306_X_OFFSET_LOWER 0
-#define SSD1306_X_OFFSET_UPPER 0
-
-// Enumeration for screen colors
-typedef enum {
-    Black = 0x00, // Black color, no pixel
-    White = 0x01  // Pixel is set. Color depends on OLED
-} SSD1306_COLOR;
 
 void I2C_init();
-
-void I2C_send(uint8_t *data, uint8_t data_size, uint16_t mem_addr);
--- Core/Src/I2C_setup.c
+++ Core/Src/I2C_setup.c
@@ -1,35 +1,7 @@
 #include "I2C_setup.h"
 
-uint8_t SSD1306_buffer[SSD1306_BUFFER_SIZE];
-
-uint8_t *init_data = {0x20,
-                    0x00,//memory addressing mode (horizontal)
-                    0xB0, //page start adress
-                    0xC8, //scan direction
-                    0x00, //set low column address
-                    0x10, //set high column address
-                    0x40, //set start line address
-                    0x81,//set contrast
-                    0xFF,
-                    0xA1, //set segment re-map 0 to 127
-                    0xA6, //normal color (0xA7 - inverse color)
-                    0xA8, // set multiplex ratio (1 to 64)
-                    0x1F, //set screen height
-                    0xA4, //output follows RAM content (0xA5 - output ignores RAM content)
-                    0xD3, //set display offset
-                    0x00, //no offset
-                    0xD5, //set display clock divide ratio/oscillator frequency
-                    0xF0, // set divide ratio
-                    0xD9, // set pre-charge period
-                    0x22,
-                    0xDA, // set com pins hardware configuration
-                    0x02,
-                    0xDB, // set vcomh
-                    0x20, // 0.77Vcc
-                    0x8D, // set DC-DC enable
-                    0x14,
-                    0xAF // on display
-                    };
+void pre_send_data_ops(uint16_t mem_addr);
+void post_send_data_ops();
 
 void I2C_init()
 {
@@ -48,12 +20,12 @@ void I2C_init()
     SET_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7);
 
     // change to I2C mode
-    MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL6, 0b0011 << GPIO_AFRL_AFSEL6_Pos);
-    MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL7, 0b0011 << GPIO_AFRL_AFSEL7_Pos);
+    MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL6, 0b0100 << GPIO_AFRL_AFSEL6_Pos);
+    MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL7, 0b0100 << GPIO_AFRL_AFSEL7_Pos);
 
     //set I2C freq = APB1 freq
     uint32_t APB1_clock = SystemCoreClock >> APBPrescTable[(RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos];
-    MODIFY_REG(I2C1->CR2, I2C_CR2_FREQ_Msk, APB1_clock);
+    MODIFY_REG(I2C1->CR2, I2C_CR2_FREQ_Msk, APB1_clock/1000000);
 
     //enable fast mode
     SET_BIT(I2C1->CCR, I2C_CCR_FS);
@@ -63,15 +35,14 @@ void I2C_init()
     
     //set I2C freq to 400k Hz
     uint32_t I2C_clock = 400000;
-    uint16_t ccr = APB1_clock/I2C_clock/3;
+    uint16_t ccr = APB1_clock/I2C_clock/3+1;
     MODIFY_REG(I2C1->CCR, I2C_CCR_CCR, ccr);
+
+    //set max Trise value
+    MODIFY_REG(I2C1->TRISE, I2C_TRISE_TRISE, (((APB1_clock/1000000) * 300U) / 1000U) + 1U);
 
     //enable peripheral
     SET_BIT(I2C1->CR1, I2C_CR1_PE);
-
-    I2C_send(init_data, sizeof(init_data)/sizeof(init_data[0]), WRITE_COMMAND_MEM_ADDR);
-
-    ssd1306_fill(Black);
 }
 
 void I2C_send(uint8_t *data, uint8_t data_size, uint16_t mem_addr)
@@ -131,17 +102,4 @@ void post_send_data_ops() {
 }
 
 
-void ssd1306_fill(SSD1306_COLOR color) {
-    for (int i = 0; i < SSD1306_BUFFER_SIZE; ++i) {
-        SSD1306_buffer[i] = color;
-    }
-}
 
-void ssd1306_update_screen() {
-    uint8_t *data = {0xB0, 0x00+SSD1306_X_OFFSET_LOWER, 0x10+SSD1306_X_OFFSET_LOWER};
-    for (uint8_t i = 0; i < SSD1306_Height/8; ++i) {
-        ++data[0];
-        I2C_send(data, 3, WRITE_COMMAND_MEM_ADDR);
-        I2C_send(&SSD1306_buffer[SSD1306_WIDTH*i], SSD1306_WIDTH, WRITE_DATA_MEM_ADDR);
-    }
-}
